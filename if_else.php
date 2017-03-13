cfef255faf9a0799b8ef15372fc28b57e49a5053
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-03-13 15:23:44 -0500

    completes next step of exercise 6.1.3

--- if_else.php
+++ if_else.php
@@ -13,10 +13,9 @@ if ($a < $b) {
 
 // TODO: Shorten these two if statements to a single if/else
 if ($b < $c) {
-    echo "$b is less than $c\n";
-}
-if ($b >= $c) {
-    echo "$b is greater than or equal to $c\n";
+    echo "$b is less than $c" . PHP_EOL;
+} else {
+    echo "$b is greater than or equal to $c" . PHP_EOL;
 }
 
 // TODO:
