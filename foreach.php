2122a1480747307e59b8834fe290fad6599aec0d
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-03-15 16:38:38 -0500

    completes next part of exercise 6.3.2

--- foreach.php
+++ foreach.php
@@ -5,3 +5,11 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 foreach ($things as $thing) {
 	fwrite(STDOUT, strtolower(gettype($thing)) . PHP_EOL);
 }
+
+fwrite(STDOUT, PHP_EOL);
+
+foreach ($things as $thing) {
+	if (is_scalar($thing)) {
+		fwrite(STDOUT, $thing);
+	}
+}
