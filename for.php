2f8738821fce00d8498bceb691d95fe12de991b1
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-03-15 14:14:55 -0500

    breaks everything

--- for.php
+++ for.php
@@ -1,11 +1,12 @@
 <?php
 
+
 fwrite(STDOUT, PHP_EOL . 'INPUT START NUMBER' . PHP_EOL);
 $start = trim(fgets(STDIN));
 
 while (!is_numeric($start)) {
 	fwrite(STDOUT, PHP_EOL . 'ERROR -- NEED NUMBER' . PHP_EOL);
-	$start = fgets(STDIN);
+	$start = trim(fgets(STDIN));
 }
 
 if ((int) $start != (double) $start) {
@@ -14,26 +15,84 @@ if ((int) $start != (double) $start) {
 
 $start = (int) $start;
 
+
+
 fwrite(STDOUT, PHP_EOL . 'INPUT END NUMBER' . PHP_EOL);
 $end = trim(fgets(STDIN));
 
-while (!is_numeric($end) or (int) $end <= $start) {
-	if ((int) $end != (double) $end) {
-		fwrite(STDOUT, PHP_EOL . 'FLOAT ACCEPTED -- ROUNDING DOWN');
-	}
-	fwrite(STDOUT, PHP_EOL . 'ERROR -- ' . (!is_numeric($end) ? 'NEED NUMBER' : 'MUST BE GREATER THAN START') . PHP_EOL);
-	$end = fgets(STDIN);
+while (!is_numeric($end)) {
+	fwrite(STDOUT, PHP_EOL . 'ERROR -- NEED NUMBER' . PHP_EOL);
+	$end = trim(fgets(STDIN));
 }
 
 if ((int) $end != (double) $end) {
-	fwrite(STDOUT, PHP_EOL . 'FLOAT ACCEPTED -- ROUNDING DOWN' . PHP_EOL);
+	fwrite(STDOUT, PHP_EOL . 'FLOAT ACCEPTED -- ROUNDING DOWN');
 }
 
 $end = (int) $end;
 
-for ($i = $start, fwrite(STDOUT, PHP_EOL . 'BEGINNING PROCESS' . PHP_EOL); $i <= $end; $i++) {
-	fwrite(STDOUT, $i . PHP_EOL);
+
+
+fwrite(STDOUT, PHP_EOL . 'INPUT INCREMENT OR LEAVE BLANK FOR DEFAULT' . PHP_EOL);
+$increment = trim(fgets(STDIN));
+
+if ((int) $increment != (double) $increment) {
+	fwrite(STDOUT, PHP_EOL . 'FLOAT ACCEPTED -- ROUNDING DOWN');
+	$increment = (int) $increment;
 }
-fwrite(STDOUT, 'ALL DONE' . PHP_EOL);
+
+$ok = false;
+
+while(!$ok) {
+	while ($increment != '' and (!is_numeric($increment) or ($increment < 0 and $start < $end) or ($increment > 0 and $start > $end) or $increment == 0)) {
+		if (!is_numeric($increment)) {
+			$error = 'NEED NUMBER';
+		} elseif ($increment < 0) {
+			$error = 'MUST BE GREATER THAN ZERO';
+		} elseif ($increment > 0) {
+			$error = 'MUST BE LESS THAN ZERO';
+		} elseif ($increment == 0) {
+			$error = 'MUST NOT BE ZERO';
+		}
+		fwrite(STDOUT, PHP_EOL . 'ERROR -- ' . $error . PHP_EOL);
+		$increment = trim(fgets(STDIN));
+		if ((int) $increment != (double) $increment) {
+			fwrite(STDOUT, 'FLOAT ACCEPTED -- ROUNDING DOWN');
+			$increment = (int) $increment;
+		}
+	}
+
+	if ($increment === '') {
+		$increment = 1;
+	}
+
+	$increment = (int) $increment;
+
+	// if (($end - $start) % $increment != 0) {
+	// 	fwrite(STDOUT, PHP_EOL . 'MAY NOT REACH END - OK?' . PHP_EOL);
+	// 	$consent = '';
+	// 	while ($consent !== 'Y' and $consent !== 'YES' and $consent !== 'N' and $consent !== 'NO') {
+	// 		fwrite(STDOUT, '(Y/N)' . PHP_EOL);
+	// 		$consent = strtoupper(trim(fgets(STDIN)));
+	// 		if ($consent === 'Y' or $consent === 'YES') {
+	// 			$ok = true;
+	// 		} else {
+	// 			fwrite(STDOUT, PHP_EOL . 'INPUT INCREMENT OR LEAVE BLANK FOR DEFAULT' . PHP_EOL);
+	// 			$increment = trim(fgets(STDIN));
+	// 		}
+	// 	}
+	// } else {
+	// 	$ok = true;
+	// }
+}
+
+echo $increment;
+
+// for ($i = $start, fwrite(STDOUT, PHP_EOL . 'BEGINNING PROCESS' . PHP_EOL); $i <= $end; $i += $increment) {
+// 	fwrite(STDOUT, $i . PHP_EOL);
+// }
+// fwrite(STDOUT, 'ALL DONE' . PHP_EOL);
+
+
 
 exit(0);
