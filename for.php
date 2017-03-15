7aad662d81be0ad6b77046b69cd348ea474c1f7f
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-03-15 15:05:00 -0500

    fixes everything

--- for.php
+++ for.php
@@ -44,7 +44,11 @@ if ((int) $increment != (double) $increment) {
 $ok = false;
 
 while(!$ok) {
-	while ($increment != '' and (!is_numeric($increment) or ($increment < 0 and $start < $end) or ($increment > 0 and $start > $end) or $increment == 0)) {
+	if ($increment === '') {
+		$increment = 1;
+	}
+	// why does adding '$increment != 0 and ' to the beginning of this conditional break it?
+	while ((!is_numeric($increment) or ($increment < 0 and $start < $end) or ($increment > 0 and $start > $end) or $increment == 0)) {
 		if (!is_numeric($increment)) {
 			$error = 'NEED NUMBER';
 		} elseif ($increment < 0) {
@@ -60,39 +64,57 @@ while(!$ok) {
 			fwrite(STDOUT, 'FLOAT ACCEPTED -- ROUNDING DOWN');
 			$increment = (int) $increment;
 		}
-	}
-
-	if ($increment === '') {
-		$increment = 1;
-	}
+		if ($increment === '') {
+			$increment = 1;
+		}
+	}	
 
 	$increment = (int) $increment;
 
-	// if (($end - $start) % $increment != 0) {
-	// 	fwrite(STDOUT, PHP_EOL . 'MAY NOT REACH END - OK?' . PHP_EOL);
-	// 	$consent = '';
-	// 	while ($consent !== 'Y' and $consent !== 'YES' and $consent !== 'N' and $consent !== 'NO') {
-	// 		fwrite(STDOUT, '(Y/N)' . PHP_EOL);
-	// 		$consent = strtoupper(trim(fgets(STDIN)));
-	// 		if ($consent === 'Y' or $consent === 'YES') {
-	// 			$ok = true;
-	// 		} else {
-	// 			fwrite(STDOUT, PHP_EOL . 'INPUT INCREMENT OR LEAVE BLANK FOR DEFAULT' . PHP_EOL);
-	// 			$increment = trim(fgets(STDIN));
-	// 		}
-	// 	}
-	// } else {
-	// 	$ok = true;
-	// }
+	if (($end - $start) % $increment != 0) {
+		fwrite(STDOUT, PHP_EOL . 'MAY NOT REACH END - OK?' . PHP_EOL);
+		$consent = '';
+		while ($consent !== 'Y' and $consent !== 'YES' and $consent !== 'N' and $consent !== 'NO') {
+			fwrite(STDOUT, '(Y/N)' . PHP_EOL);
+			$consent = strtoupper(trim(fgets(STDIN)));
+			if ($consent === 'Y' or $consent === 'YES') {
+				$ok = true;
+			} else {
+				fwrite(STDOUT, PHP_EOL . 'INPUT INCREMENT OR LEAVE BLANK FOR DEFAULT' . PHP_EOL);
+				$increment = trim(fgets(STDIN));
+			}
+		}
+	} else {
+		$ok = true;
+	}
 }
 
 echo $increment;
 
-// for ($i = $start, fwrite(STDOUT, PHP_EOL . 'BEGINNING PROCESS' . PHP_EOL); $i <= $end; $i += $increment) {
-// 	fwrite(STDOUT, $i . PHP_EOL);
-// }
-// fwrite(STDOUT, 'ALL DONE' . PHP_EOL);
+for ($i = $start, fwrite(STDOUT, PHP_EOL . 'BEGINNING PROCESS' . PHP_EOL); $i <= $end; $i += $increment) {
+	fwrite(STDOUT, $i . PHP_EOL);
+}
+fwrite(STDOUT, 'ALL DONE' . PHP_EOL);
 
 
 
 exit(0);
+
+
+
+// echo ((1.4 - 1.2) / .1);
+//
+// ** this returns 2 **
+//
+//
+// $double = ((1.4 - 1.2) / .1);
+// var_dump($double);
+//
+// ** this returns double(2) **
+//
+//
+// var_dump((int) $double);
+//
+// ** this returns int(1)**
+//
+// Why?
