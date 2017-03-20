f61065494f9d334f118dbab56a4b93fdf7e5d782
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-03-20 09:08:46 -0500

    finishes first part of arithmetic exercise

--- arithmetic.php
+++ arithmetic.php
@@ -2,22 +2,49 @@
 
 function add($a, $b)
 {
-    return $a + $b;
+	return $a + $b;
 }
 
 function subtract($a, $b)
 {
-    // Add code here
+	return $a - $b;
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+	$result = 0;
+	$a *= $b / abs($b);
+	$b = abs($b);
+
+	for ($i = 0; $i < $b; $i++) { 
+		$result += $a;
+	}
+
+	return $result;
 }
 
 function divide($a, $b)
 {
-    // Add code here
+	$result = '';
+	$r = $a;
+	$i = 0;
+	while (multiply(((float) $result), $b) < $a) {
+		$value = 0;
+		while ($i <= $r) {
+			if ($i + $b <= $r) {
+				$value++;
+			} else {
+				$result .= "$value";
+				if ($r != 0 and !strpos($result, '.')) {
+					$result .= '.';
+				}
+				$r -= $i;
+				$r *= 10;
+				break;
+			}
+			$i += $b;
+		}
+		$i = 0;
+	}
+	return $result;
 }
-
-// Add code to test your functions here
