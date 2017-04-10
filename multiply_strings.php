05348ce245e5d22901ebff8b46ed2c5df0fa6f7c
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-09 19:09:14 -0500

    fixes everything, unary stuff is no longer used

--- multiply_strings.php
+++ multiply_strings.php
@@ -9,12 +9,10 @@ function multiply($a, $b)
 
 	$decimalPlaces = rememberDecimals($a) + rememberDecimals($b);
 
-	$a = decimalToUnary(strrev($a));
-	$b = decimalToUnary(strrev($b));
+	$a = strrev($a);
+	$b = strrev($b);
 
-	$uResult = multiplyUnary($a . $b);
-
-	$result = unaryToDecimal($uResult);
+	$result = multiplyStrings($a, $b);
 
 	$result = recallDecimals($result, $decimalPlaces);
 
@@ -67,159 +65,76 @@ function removeExtraZeroes($str)
 	return preg_replace(['#^0*(\d+)#', '#(?:\.(\d*[1-9]+)*0+)$#', '#\.$#'], ['$1', '.$1', ''], $str);
 }
 
-function decimalToUnary($str)
+function increment($x)
 {
-	$uStr = preg_replace(['#0#', '#1#', '#2#', '#3#', '#4#', '#5#', '#6#', '#7#', '#8#', '#9#', ], [',.', ',1.', ',11.', ',111.', ',1111.', ',11111.', ',111111.', ',1111111.', ',11111111.', ',111111111.'], $str);
-	$uStr = '?' . $uStr . '!';
-	return $uStr;
-}
-
-function unaryToDecimal($str)
-{
-	$dStr = preg_replace(['#,1{9}\.#', '#,1{8}\.#', '#,1{7}\.#', '#,1{6}\.#', '#,1{5}\.#', '#,1{4}\.#', '#,1{3}\.#', '#,1{2}\.#', '#,1\.#', '#,\.#', '#[!\?]#'], ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0', ''], $str);
-	return $dStr;
-}
-
-function increment(&$x)
-{
-	switch ($x) {
+	switch ($x[0]) {
 		case '0':
-			$x = '1';
+			$x[0] = '1';
 			break;
 		case '1':
-			$x = '2';
+			$x[0] = '2';
 			break;
 		case '2':
-			$x = '3';
+			$x[0] = '3';
 			break;
 		case '3':
-			$x = '4';
+			$x[0] = '4';
 			break;
 		case '4':
-			$x = '5';
+			$x[0] = '5';
 			break;
 		case '5':
-			$x = '6';
+			$x[0] = '6';
 			break;
 		case '6':
-			$x = '7';
+			$x[0] = '7';
 			break;
 		case '7':
-			$x = '8';
+			$x[0] = '8';
 			break;
 		case '8':
-			$x = '9';
+			$x[0] = '9';
 			break;
 		case '9':
-			$x = '0';
-			return true;
+			$x[0] = '0';
+			isset($x[1]) ?: $x .= '0';
+			$x = $x[0] . increment(substr($x, 1));
+			break;
 	}
-	return false;
+	return $x;
 }
 
-function strposX($haystack, $needle, $number)
+function addStrings($a, $b)
 {
-	$skipped = '';
-	$pos = strpos($haystack, $needle);
-	$newHaystack = $haystack;
-	for ($i = 1; $i < $number; $i++) {
-		$newHaystack = substr($newHaystack, strpos($newHaystack, $needle));
-		$skipped .= substr($haystack, 0, strlen($haystack) - strlen($newHaystack));
-		// $pos += 1;
-		// $pos += strpos($haystack, $needle, $pos) - 1;
-		$haystack = $newHaystack;
-		// echo $pos . PHP_EOL;
-	}
-	return strpos($haystack, $needle, strlen($skipped));
-	// if ($number == '1') {
-	// 	return strpos($haystack, $needle);
-	// } elseif ($number > '1') {
-	// 	return strpos($haystack, $needle, strposX($haystack, $needle, $number - 1) + strlen($needle));
-	// } else {
-	// 	return error_log('Error: Value for parameter $number is out of range');
-	// }
-}
-
-function substrXY($str, $start, $end)
-{
-	$length = strlen(substr($str, $end));
-	return substr($str, $start, -$length);
-}
-
-function multiplyUnary($str)
-{
-	$a = substrXY($str, 0, strpos($str, '!') + 1);
-	$b = substr($str, strposX($str, '?', 2));
-	$addStr = '';
-
-	if ($a === '?,.!' or $b === '?,.!') return '?,.!';
-	if ($a === '?,1.!') return $b;
-	if ($b === '?,1.!') return $a;
-
-	for ($i = '?,.!'; $i !== $b; $i = uPlusPlus($i)) {
-		$addStr .= $a;
-	}
-	return addUnary($addStr);
-}
-
-function uPlusPlus($str)
-{
-	$one = '?,1.!';
-
-	while (getDigitCount($one, 1) < getDigitCount($str, 1)) {
-		$one = substr($one, 0, -1) . ',.!';
-	}
-	echo $str . $one . PHP_EOL;
-	return addUnary($str . $one);
-}
-
-function addUnary($str)
-{
-	$result = '?';
-	$numbers = getNumberCount($str);
-	$maxDigits = getMaxDigits($str);
-	$digit = '';
-	$carry = '';
-	for ($i = 1; $i <= $maxDigits; $i++) {
-		for ($j = 1; $j <= $numbers; $j++) {
-			$currentNumber = substrXY($str, strposX($str, '?', $j) + 1, strposX($str, '!', $j));
-			$digit .= substrXY($currentNumber, strposX($currentNumber, ',', $i) + 1, strposX($currentNumber, '.', $i));
+	padStrings($a, $b);
+	$result = $a;
+	for ($i = 0; $i < strlen($a); $i++) {
+		$segment = substr($result, $i);
+		for ($j = '0'; $j !== substr($b, $i, 1); $j = increment($j)) {
+			$segment = increment($segment);
 		}
-		$digit .= $carry;
-		$carry = '';
-		while (strlen($digit) > 9) {
-			$carry .= '1';
-			$digit = substr($digit, 10);
-		}
-		$result .= ',' . $digit . '.';
-		$digit = '';
+		$result = substr($result, 0, $i) . $segment;
 	}
-	if ($carry) $carry = ',' . $carry . '.';
-	$result .= $carry . '!';
 	return $result;
 }
 
-function addStrings($str)
-{}
-
-function getNumberCount($str)
+function multiplyStrings($a, $b)
 {
-	return preg_match_all('#\?#', $str);
-}
-
-function getDigitCount($str, $num)
-{
-	return preg_match_all('#,#', substrXY($str, strposX($str, '?', $num), strposX($str, '!', $num)));
-}
-
-function getMaxDigits($str)
-{
-	$numbers = getNumberCount($str);
-	$max = 0;
-
-	for ($i = 1; $i <= $numbers; $i++) {
-		$digits = getDigitCount($str, $i);
-		if ($digits > $max) $max = $digits;
+	$result = $a;
+	if ($a === '0' or $b === '0') return '0';
+	if ($a === '1') return $b;
+	for ($i = '1'; $i !== $b; $i = increment($i)) {
+		$result = addStrings($result, $a);
 	}
-	return $max;
+	return $result;
+}
+
+function padStrings(&$a, &$b)
+{
+	while (strlen($a) < strlen($b)) {
+		$a .= '0';
+	}
+	while (strlen($b) < strlen($a)) {
+		$b .= '0';
+	}
 }
